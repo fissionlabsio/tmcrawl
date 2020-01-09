413ae509becb0acb847453c0a6599beaacedf014
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-01-09 12:19:50 -0500

    Add DB tests

--- db/db.go
+++ db/db.go
@@ -28,7 +28,7 @@ type (
 // It will create all the necessary Badger DB buckets if they don't already exist.
 func NewBadgerDB(dataDir, dbName string) (DB, error) {
 	dbPath := filepath.Join(dataDir, dbName)
-	db, err := badger.Open(badger.DefaultOptions(dbPath))
+	db, err := badger.Open(badger.DefaultOptions(dbPath).WithEventLogging(false))
 	if err != nil {
 		return nil, err
 	}
